bdbf38ebecfd85a282bd3da4a32704eee4aa72d2
rosenstam <98087002+rosenstam@users.noreply.github.com>
2022-02-16 16:19:14 +0100

    Create Main.java
    Skriven  i Githubs textredigerare

---
+++ Main.java
@@ -0,0 +1,8 @@
+Public class Main {
+  
+  public static void main(String[] args) {
+    
+    System.out.println("Denna repo skapades utan licens, hoppas jag inte blir stämd av någon");
+    
+  }
+}
